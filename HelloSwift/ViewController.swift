1c8853157f13fb44e277582b6d6464e679700bff
Alex Butiev <butievaleksei@mail.ru>
2022-03-26 11:53:25 +0300

    setup view

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloSwiftLabel: UILabel!
+    @IBOutlet var showTextLabel: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func showTextButton() {
+        helloSwiftLabel.isHidden = true
+        
+    }
+    
 }
 
